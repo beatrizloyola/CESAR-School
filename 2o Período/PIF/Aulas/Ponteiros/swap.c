92a5869f2d51d8703a82eebb24211db2451a35f9
Beatriz Loyola <beatrizvasconcelos0802@gmail.com>
2026-03-12 09:14:00 -0300

    Função Swap com Ponteiros

---
+++ 2o Período/PIF/Aulas/Ponteiros/swap.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void swap(int *refA, int *refB){
+    int temporaria = *refA;
+    *refA = *refB;
+    *refB = temporaria;
+}
+
+int main(){
+    int a = 10;
+    int b = 15;
+
+    int *refA;
+    refA = &a;
+    int *refB;
+    refB = &b;
+    
+    swap(refA, refB);
+    printf("A: %d\nB: %d", a, b);
+
+    return 0;
+}
