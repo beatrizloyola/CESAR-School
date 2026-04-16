a75d34889f837ac80a3ef34346d09180f07ad5d7
Beatriz Loyola <beatrizvasconcelos0802@gmail.com>
2026-04-16 09:45:32 -0300

    MatrizBlocoContinuo.c

---
+++ 2o Período/PIF/Listas/Lista Revisão Structs Malu/MatrizBlocoContinuo.c
@@ -0,0 +1,20 @@
+/*
+
+Matriz em bloco contínuo
+Escreva um programa que:
+• leia n e m
+• aloque dinamicamente uma matriz usando um único bloco contínuo de memória
+• leia os valores da matriz
+• imprima a matriz
+• calcule a soma de todos os elementos
+• libere a memória
+
+*/
+
+int main(){
+    int n, m;
+    scanf("%d %d", n, m);
+    int **matriz = (**int)malloc()
+
+    return 0;
+}
